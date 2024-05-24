eb91259f7c5edb5372c1917f35f4de4ff58addad
Mostakimw <scientist.expert@gmail.com>
2024-05-24 11:39:03 +0600

    material ui done

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 const Homepage = () => {
   return (
     <div>
       <h1>This is Homepage component</h1>
+      <Button variant="contained">Click</Button>
     </div>
   );
 };
